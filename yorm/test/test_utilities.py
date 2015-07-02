f9cb79f832364a02eace13d9126cf327eadc6541
Jace Browning <jacebrowning@gmail.com>
2015-07-01 21:36:52 -0400

    Add failing signature tests

--- yorm/test/test_utilities.py
+++ yorm/test/test_utilities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# pylint:disable=R0201
+# pylint:disable=W0621,R,C
 
 """Unit tests for the `utilities` module."""
 
@@ -98,6 +98,50 @@ class TestSyncObject:
         with patch('yorm.common.read_text', Mock(return_value="abc: 123")):
             sample = utilities.sync(self.Sample(), "sample.yml")
         assert 123 == sample.abc
+
+
+class SampleWithMagicMethods:
+
+    """Sample class with magic methods implemented."""
+
+    def __init__(self):
+        self.values = []
+
+    def __setattr__(self, name, value):
+        self.values.append(('__setattr__', name, value))
+        super().__setattr__(self, name, value)
+
+
+@pytest.fixture
+def unmapped():
+    return SampleWithMagicMethods()
+
+
+@pytest.yield_fixture
+def mapped():
+    import yorm
+    yorm.settings.fake = True
+    yield utilities.sync(SampleWithMagicMethods(), "sample.yml")
+    yorm.settings.fake = False
+
+
+class TestSyncObjectMaintainsSignature:
+
+    def test_repr(self, unmapped, mapped):
+        assert repr(unmapped) == repr(mapped)
+
+    def test_doc(self, unmapped, mapped):
+        assert unmapped.__doc__ == mapped.__doc__
+
+    def test_class_name(self, unmapped, mapped):
+        assert unmapped.__class__.__name__ == mapped.__class__.__name__
+
+    def test_instance(self, unmapped, mapped):
+        assert isinstance(mapped, unmapped.__class__)
+
+    def test_magic_methods(self, mapped):
+        setattr(mapped, 'value', 42)
+        assert mapped.values == [('__setattr__', 'value', 42)]
 
 
 @patch('yorm.common.create_dirname', Mock())
